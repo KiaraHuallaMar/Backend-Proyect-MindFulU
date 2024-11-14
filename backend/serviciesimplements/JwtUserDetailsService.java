64849ffde5791a97a711e9edf745a73a8f69b95a
aldairpc15 <u202020356@upc.edu.pe>
2024-11-13 19:23:37 -0500

    JwtUserDetailsService añadido

---
+++ backend/serviciesimplements/JwtUserDetailsService.java
@@ -0,0 +1,42 @@
+package edu.com.upc.minfulu.dbprojectmindfulu.serviciesimplements;
+
+import edu.com.upc.minfulu.dbprojectmindfulu.entities.User;
+import edu.com.upc.minfulu.dbprojectmindfulu.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Clase 2
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findOneByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("User not exists", username));
+        }
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+
+        user.getRoles().forEach(rol -> {
+            roles.add(new SimpleGrantedAuthority(rol.getRole()));
+        });
+
+        UserDetails ud = new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getEnabled(), true, true, true, roles);
+
+        return ud;
+    }
+}
