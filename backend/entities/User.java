90af73153c582350d0a52281f43760345b32f4c1
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2024-11-13 19:17:48 -0500

    entitie

---
+++ backend/entities/User.java
@@ -0,0 +1,37 @@
+package edu.com.upc.minfulu.dbprojectmindfulu.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 30, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 200)
+    private String password;
+
+    private Boolean enabled;
+
+    @JsonIgnore
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private List<Role> roles;
+}
