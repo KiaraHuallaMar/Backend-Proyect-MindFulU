47ce0491f05fd155a45ba22dcced93ee4b72dffe
aldairpc15 <u202020356@upc.edu.pe>
2024-11-19 15:13:03 -0500

    Actualizacion  User.java
    Actualizacion

--- backend/entities/User.java
+++ backend/entities/User.java
@@ -1,5 +1,6 @@
 package edu.com.upc.minfulu.dbprojectmindfulu.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,7 +17,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "users")
 
-public class User implements Serializable{
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
