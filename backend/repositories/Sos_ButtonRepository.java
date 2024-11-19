951b759b04c850198de778d7de98052c928dde70
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2024-11-19 03:06:16 -0500

    Delete backend/repositories directory

--- backend/repositories/Sos_ButtonRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.com.upc.minfulu.dbprojectmindfulu.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import edu.com.upc.minfulu.dbprojectmindfulu.entities.Sos_Button;
-
-@Repository
-public interface Sos_ButtonRepository extends JpaRepository<Sos_Button, Integer> {
-}
--- backend/repositories/Support_GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.com.upc.minfulu.dbprojectmindfulu.repositories;
-
-import edu.com.upc.minfulu.dbprojectmindfulu.entities.Support_Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Support_GroupRepository extends JpaRepository<Support_Group,Integer> {
-}
