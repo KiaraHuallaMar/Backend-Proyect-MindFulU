43f59831307e411e433683645f46c788ecd079c7
aldairpc15 <u202020356@upc.edu.pe>
2024-11-19 15:37:22 -0500

    Implementacion de Home
    Implementacion de Home

---
+++ frontend/src/app/component/home/home.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectorRef, Component, OnInit, ViewChild  } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators, FormBuilder, FormsModule} from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports:  [MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule, MatIconModule, MatSidenavModule, MatFormFieldModule, ReactiveFormsModule, MatSelectModule, MatInputModule, CommonModule, MatButtonModule, RouterLink, MatDatepickerModule, MatInputModule, MatNativeDateModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  @ViewChild(MatSidenav)
+  sidenav!:MatSidenav;
+
+  constructor(private observer: BreakpointObserver, private cd:ChangeDetectorRef, private router:Router){
+
+  }
+  editarusuario(){
+   this.router.navigateByUrl('/users');
+  }
+
+}
